09170a47cf8306db38e56384d1dcc7e6e057ca6c
UnrelatedString <unrelatedstring@gmail.com>
2025-04-29 02:19:45 -0400

    HOW MUCH CAN NIX HURT ME also why is Show (Array a) even foreign I guess because there's no intercalate in Prelude but yikes

--- test.lua
+++ test.lua
@@ -1,3 +1,5 @@
 -- fix for compiler typo
 os.execute("sed 's/patch_v = patches/patch_v = patches[k]/' output/test.lua > output/test_.lua")
-dofile("output/test_.lua").main()
+-- fix for broken eta rewrite on latest release because CI can't use the flake without timing out
+os.execute("sed 's/M.Test_QuickCheck_Gen_state(M.Test_QuickCheck_Gen_runGen(f(s.size)))/M.Test_QuickCheck_Gen_state(function(s)return M.Test_QuickCheck_Gen_runGen(f(s.size))end)/' output/test_.lua > output/test__.lua")
+dofile("output/test__.lua").main()
