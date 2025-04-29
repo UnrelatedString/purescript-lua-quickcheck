218c2f60f868cc8f1329dc3be46c913acc71c693
UnrelatedString <unrelatedstring@gmail.com>
2025-04-29 01:52:36 -0400

    and ofc I also need the Prelude fix I wrote to find out. there's another issue with Generic.

--- test.lua
+++ test.lua
@@ -1,1 +1,3 @@
-dofile("output/test.lua").main()
+-- fix for compiler typo
+os.execute("sed 's/patch_v = patches/patch_v = patches[k]/' output/test.lua > output/test_.lua")
+dofile("output/test_.lua").main()
